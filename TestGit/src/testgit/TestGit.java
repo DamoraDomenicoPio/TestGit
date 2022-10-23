ed4b322f8058645a37b8e6bae3769aa529f8c9fb
Cuomo Ferdinando <Cuomo Ferdinando@DESKTOP-5FL2QF9>
2022-10-23 17:56:16 +0200

    Ho fatto il commit

--- TestGit/src/testgit/TestGit.java
+++ TestGit/src/testgit/TestGit.java
@@ -16,6 +16,7 @@ public class TestGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("AAAAAA");
+        System.out.println("e zitt");
     }
     
 }
