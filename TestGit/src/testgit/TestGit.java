5ce3340104db341dd4d839a3be648c0e415bfe2e
DOMENICO PIO D'AMORA <domypio001@gmail.com>
2022-10-23 17:51:56 +0200

    Prova push

--- TestGit/src/testgit/TestGit.java
+++ TestGit/src/testgit/TestGit.java
@@ -15,7 +15,7 @@ public class TestGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("AAAAAAAAAAAA");
+        System.out.print("AAAAAA");
     }
     
 }
