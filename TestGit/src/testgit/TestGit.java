a6a0d18cc3a471c8d1322e4a8b3b6f69c6340473
DOMENICO PIO D'AMORA <domypio001@gmail.com>
2022-10-23 17:10:28 +0200

    Initial import

--- TestGit/src/testgit/TestGit.java
+++ TestGit/src/testgit/TestGit.java
@@ -15,7 +15,6 @@ public class TestGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello world!");
     }
     
 }
